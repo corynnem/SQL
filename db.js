8cc7d0e5753f170aac5bbdf0534a286c287b2053
Corynne Moody <cmoody@elevenfifty.org>
2022-01-15 18:16:57 -0500

    getting heroku app crashed error, added ssl object in node-postgres docs

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 const { Pool } = require('pg')
 const config = {
   connectionString: `postgres://postgres:${process.env.PASS}@localhost:5432/mydb`,
-  ssl: process.env.ENVIRONMENT === 'production'
+  ssl:{
+    rejectUnauthorized: false,
+    ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString(),
+    key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
+    cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),
+  }
 }
 
 // Pooling allows for a reusable 'pool' of clients to be checked out, used and returned in less time
