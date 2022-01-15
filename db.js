4160c6e3e5d7dd50c6c0904c229efdca8c170169
Corynne Moody <cmoody@elevenfifty.org>
2022-01-15 18:30:07 -0500

    ssl object added from node-postgres docs

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 const { Pool } = require('pg')
+const fs = require('fs')
 const config = {
-  connectionString: `postgres://postgres:${process.env.PASS}@localhost:5432/mydb?sslmode=require`,
-  ssl: process.env.ENVIRONMENT === 'production'
+  connectionString: `postgres://postgres:${process.env.PASS}@localhost:5432/mydb`,
+  ssl: {
+    rejectUnauthorized: false,
+    ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString(),
+    key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
+    cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),
+  },
 }
 
 // Pooling allows for a reusable 'pool' of clients to be checked out, used and returned in less time
