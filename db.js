c451752add9903d286490e0346b7f3508091a816
Corynne Moody <cmoody@elevenfifty.org>
2022-01-15 18:32:51 -0500

    added dialect of postgres

--- db.js
+++ db.js
@@ -2,12 +2,8 @@ const { Pool } = require('pg')
 const fs = require('fs')
 const config = {
   connectionString: `postgres://postgres:${process.env.PASS}@localhost:5432/mydb`,
-  ssl: {
-    rejectUnauthorized: false,
-    ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString(),
-    key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
-    cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),
-  },
+  dialect: 'postgres',
+  ssl: process.env.ENVIRONMENT === 'production'
 }
 
 // Pooling allows for a reusable 'pool' of clients to be checked out, used and returned in less time
