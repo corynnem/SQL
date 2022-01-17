a38f9698444b87a864e853c3cca4ec3ae3da8fb2
Corynne Moody <cmoody@elevenfifty.org>
2022-01-17 16:54:11 -0500

    changed pool to client

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 const { Pool } = require('pg')
 const { parseSsl } = require('pg-ssl')
-const fs = require('fs')
-const config = {
-  connectionString: `postgres://postgres:${process.env.PASS}@localhost:5432/mydb`,
-  // ssl: parseSsl()
-}
 
+let client = new Client({
+  user: "postgres",
+  password: process.env.PASS,
+  database: "mydb",
+  port: 5432,
+  host: "localhost",
+  ssl: true
+});
+client.connect(function () {
+  console.log("connected");
+});
 
 // Pooling allows for a reusable 'pool' of clients to be checked out, used and returned in less time
 const pool = new Pool(config)
