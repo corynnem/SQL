2c3b4efbd03b7b6bb2483287ed9741154d622b72
Corynne Moody <cmoody@elevenfifty.org>
2022-01-17 17:04:39 -0500

    reconfigured pool

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
-const { Client } = require('pg')
+const { Client, Pool } = require('pg')
 const { parseSsl } = require('pg-ssl')
 
-let client = new Client({
+let pool = new Pool({
   user: "postgres",
   password: process.env.PASS,
   database: "mydb",
   port: 5432,
   host: "localhost",
-  ssl: true
+  ssl: process.env.ENVIROMENT === 'production' ? true : false
 });
-client.connect(function () {
-  console.log("connected");
-});
+// client.connect(function () {
+//   console.log("connected");
+// });
 
 // Pooling allows for a reusable 'pool' of clients to be checked out, used and returned in less time
-// const pool = new Pool(config)
+const client = new Pool(pool)
 
 module.exports = { client }
