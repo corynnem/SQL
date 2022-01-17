7b3a02e75e21921aa3f6bc2e309c5f3f0b9a7656
Corynne Moody <cmoody@elevenfifty.org>
2022-01-17 16:50:00 -0500

    removed all ssl

--- db.js
+++ db.js
@@ -3,8 +3,9 @@ const { parseSsl } = require('pg-ssl')
 const fs = require('fs')
 const config = {
   connectionString: `postgres://postgres:${process.env.PASS}@localhost:5432/mydb`,
-  ssl: parseSsl()
+  // ssl: parseSsl()
 }
+
 
 // Pooling allows for a reusable 'pool' of clients to be checked out, used and returned in less time
 const pool = new Pool(config)
