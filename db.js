eb49b0b1583820b842ed6201d5103ad157c01b9f
Corynne Moody <cmoody@elevenfifty.org>
2022-01-17 17:45:59 -0500

    added pool connection

--- db.js
+++ db.js
@@ -6,5 +6,6 @@ const config = {
 
 // Pooling allows for a reusable 'pool' of clients to be checked out, used and returned in less time
 const client = new Pool(config)
+client.connect()
 
 module.exports = { client }
