6dc59d49ee4473d13e97ddd5c6cf1f7246c47d07
Corynne Moody <cmoody@elevenfifty.org>
2022-01-17 17:35:00 -0500

    .

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const { users, books } = require('./controllers');
 
 
 app.use(Express.json())
-
-
 app.use(cors)
+
 app.use('/user', users)
 app.use('/books', books)
+
+app.use("/static", Express.static("node_modules"));
+
 
 app.listen(process.env.PORT, () => {  
     console.log(`app listening on ${process.env.PORT}`)
