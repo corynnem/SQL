02b3eb6641c1bd73a1a40aab1ac17938c6b669a4
Corynne Moody <cmoody@elevenfifty.org>
2022-01-17 17:42:52 -0500

    removed ssl mode

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg')
 const fs = require('fs')
 const config = {
-  connectionString: `postgres://postgres:${process.env.PASS}@localhost:5432/mydb?sslmode=require`,
+  connectionString: `postgres://postgres:${process.env.PASS}@localhost:5432/mydb`,
 }
 
 // Pooling allows for a reusable 'pool' of clients to be checked out, used and returned in less time
