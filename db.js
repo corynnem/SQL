a62b088ec1754a0a591d3bbbc49068c3728d6f14
Corynne Moody <cmoody@elevenfifty.org>
2022-01-18 17:37:37 -0500

    changed start script to node app.js instead of nodemon command

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
 const { Pool } = require('pg')
-const fs = require('fs')
 const config = {
-  connectionString: `postgres://postgres:${process.env.PASS}@localhost:5432/mydb`,
+  connectionString: `postgres://postgres:${process.env.PASS}@localhost:5432/mydb?sslmode=require`,
 }
 
 // Pooling allows for a reusable 'pool' of clients to be checked out, used and returned in less time
 const client = new Pool(config)
-client.connect()
+
 
 module.exports = { client }
+
+
+// At the bottom is ssl configuration
+// https://devcenter.heroku.com/articles/connecting-heroku-postgres
+
+// db creds
+// https://data.heroku.com/datastores/225366df-e15d-43b2-a802-9b62e8cd40b7#administration
+
+// config vars
+// https://dashboard.heroku.com/apps/book-db-sql/settings
+
