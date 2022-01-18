4acad58d6272a27b62ea8e85fb6235b4ba2aec5d
Corynne Moody <cmoody@elevenfifty.org>
2022-01-18 18:01:57 -0500

    removed rogue comma in db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg')
 
 
-const connectionString = `postgresql://postgres:${process.env.PASS}@localhost:5432/mydb`,
+const connectionString = `postgresql://postgres:${process.env.PASS}@localhost:5432/mydb`
 
 // Pooling allows for a reusable 'pool' of clients to be checked out, used and returned in less time
 const pool = new Pool({connectionString})
