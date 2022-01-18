178dfeab01e531709d8464bcca764cbf84b45d12
Corynne Moody <cmoody@elevenfifty.org>
2022-01-18 17:47:58 -0500

    removed sslmode from connection string

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg')
 const config = {
-  connectionString: `postgres://postgres:${process.env.PASS}@localhost:5432/mydb?sslmode=require`,
+  connectionString: `postgres://postgres:${process.env.PASS}@localhost:5432/mydb`,
 }
 
 // Pooling allows for a reusable 'pool' of clients to be checked out, used and returned in less time
